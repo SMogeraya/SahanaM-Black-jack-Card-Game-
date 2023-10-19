80f7a28ab74285c862164dbc404f2da3c196dd57
SMogeraya <122043330+SMogeraya@users.noreply.github.com>
2023-10-19 16:42:40 +0530

    Update bj.js

--- bj.js
+++ bj.js
@@ -56,9 +56,23 @@ btn.addEventListener("click",() =>{
 
 
 function fun(){
-   
-    location.reload();
+    var page  =document.getElementById("page");
+    page.classList.toggle('active');
+    
+    var popup =document.getElementById("popup");
+    popup.classList.toggle('active');
+    
 }
+function fun1(){
+    var page  =document.getElementById("page");
+    page.classList.toggle('active');
+    
+    var popup =document.getElementById("popup");
+    popup.classList.toggle('active');
+    location.reload();
+    
+}
+
 
 function start()
 {a+=1;
@@ -68,11 +82,56 @@ function start()
     });
     count_of_cards=0;
     if(count_of_chance>=6)
-    {
+    { 
         const count_of_cards=1;
         document.getElementById("cards").textContent="";
         document.getElementById("sum").textContent="";
         document.getElementById("countt").textContent="Total Wins:"+" "+count;
+       
+        if(count==1)
+        {document.getElementById("countt1").textContent="CONGRATULATIONS!!!\n\n Total Wins:1";
+           image=document.getElementById("image").src="https://www.freeiconspng.com/uploads/celebration-icon-png-12.png";
+           image.style.width="20px";
+           image.style.height="20px";
+
+        }
+        else if(count==2)
+        {
+            document.getElementById("countt1").textContent="Wow!!!Amazing!! \n Total Wins:2";
+            image=document.getElementById("image").src="https://www.pinpng.com/pngs/b/290-2908286_party-emoji-png.png";
+            image.style.width="20px";
+            image.style.height="20px";
+        }
+
+        else if(count==3)
+        {
+            document.getElementById("countt1").textContent="You killed it!!! \n Total Wins:3";
+            image=document.getElementById("image").src="https://clipartmag.com/images/3-stars-clipart-22.png";
+            image.style.width="42px";
+            image.style.height="20px";
+        }
+        else if(count==4)
+        {
+            document.getElementById("countt1").textContent="You are Unstoppable!!!!! \n\n Total Wins:4";
+            image=document.getElementById("image").src="https://th.bing.com/th/id/R.c40fad220c289e6ba6d1cf019b4d2c93?rik=uyAMLGVX7Av0lg&riu=http%3a%2f%2fwww.cyberlink.com%2fstat%2fproduct%2ffree-zone%2fenu%2fimg%2fmedal.png&ehk=kYm1nqPnyWDTfnblPnryO8OTrRTwLjGLNL4V9nYvbtM%3d&risl=&pid=ImgRaw&r=0";
+            image.style.width="16px";
+            image.style.height="26px";
+        }
+        else if(count==5)
+        {
+            document.getElementById("countt1").textContent="Unbeatable!!! \n Total Wins:5";
+            image=document.getElementById("image").src="https://www.pngplay.com/wp-content/uploads/8/Winner-PNG-Photos.png";
+            image.style.width="20px";
+            image.style.height="20px";
+        }
+        
+        else{
+            document.getElementById("countt1").textContent="BETTER LUCK NEXT TIME";
+            image=document.getElementById("image").src="https://cdn3d.iconscout.com/3d/premium/thumb/all-the-best-5796797-4862985.png";
+            image.style.width="20px";
+            image.style.height="20px";
+        
+        }
     }
    
     
@@ -110,21 +169,7 @@ function start()
         count_of_chance+=1;}
     else{
         
-        /*const btn=document.getElementById("start");
-    const ndivi=document.getElementById('winsdiv');
-    const odivi=document.getElementById('begin');
-  {      
-    odivi.style.display='none';
-    ndivi.style.transform='translate(-50%,-30%) scale(1)';
-    if(count==0)
-    { 
-            document.getElementById('winsp').textContent="Better Luck Next Time";
-    }
-    else
-    {
-            document.getElementById('winsp').textContent="You won";
-    }
-} */ 
+
     alert("Chances over...Start a new game");
     }
 }
